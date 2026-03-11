084abc9900db72210791fe7a54f4fcf6fb8c9ed3
Ahn JoongHoo <gerrard1219@gmail.com>
2026-03-11 17:06:54 +0900

    Rename : coffee module to caffeine module

--- src/modules/coffee/coffee.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { CoffeeService } from './coffee.service';
-
-@Controller('coffee')
-export class CoffeeController {
-  constructor(private readonly coffeeService: CoffeeService) {}
-}
