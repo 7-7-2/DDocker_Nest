008c098c1064ed41029cc697f4ff6650241ddd70
Ahn JoongHoo <gerrard1219@gmail.com>
2026-03-24 21:58:33 +0900

    Feat : search users

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
+import { SearchRepository } from './search.repository';
 
 @Module({
   controllers: [SearchController],
-  providers: [SearchService],
+  providers: [SearchService, SearchRepository],
 })
 export class SearchModule {}
