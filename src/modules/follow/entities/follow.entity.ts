2acc205b2a498f6516ce05ba0a448aa75ac25eee
Ahn JoongHoo <gerrard1219@gmail.com>
2026-03-27 17:11:08 +0900

    Feat : align entity models with new database columns

--- src/modules/follow/entities/follow.entity.ts
+++ src/modules/follow/entities/follow.entity.ts
@@ -2,5 +2,6 @@ export interface FollowRow {
   id: number;
   following_user_id: string;
   followed_user_id: string;
+  is_mutual: number;
   created_at: Date;
 }
--- src/modules/user/entities/user-stats.entity.ts
+++ src/modules/user/entities/user-stats.entity.ts
@@ -2,4 +2,7 @@ export interface UserStatsRow {
   id: number;
   user_id: string;
   sum: number;
+  post_count: number;
+  following_count: number;
+  follower_count: number;
 }
