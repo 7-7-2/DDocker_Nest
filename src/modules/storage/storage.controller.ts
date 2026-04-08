f4e520b9185faa3a78938be0ce7b9e921d9ebdd7
Ahn JoongHoo <gerrard1219@gmail.com>
2026-04-08 13:19:51 +0900

    Fix : user profile route update

--- src/modules/storage/storage.controller.ts
+++ src/modules/storage/storage.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Delete, Param, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Delete,
+  Param,
+  UseGuards,
+  Query,
+} from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 import { GetUser } from '../../auth/decorators/get-user.decorator';
@@ -11,13 +18,13 @@ export class StorageController {
 
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @Get('upload/:dir/:userId/:postId')
+  @Get('upload/:dir/:userId')
   @ApiOperation({ summary: 'R2 Presigned Upload URL 생성' })
   async getUploadUrl(
     @GetUser('public_id') loggedInUserId: string,
     @Param('dir') dir: string,
     @Param('userId') userId: string,
-    @Param('postId') postId?: string,
+    @Query('postId') postId?: string,
   ): Promise<{ url: string }> {
     return await this.storageService.getUploadUrl(
       loggedInUserId,
@@ -29,13 +36,13 @@ export class StorageController {
 
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
-  @Get('delete/:dir/:userId/:postId')
+  @Get('delete/:dir/:userId')
   @ApiOperation({ summary: 'R2 Presigned Delete URL 생성' })
   async getDeleteUrl(
     @GetUser('public_id') loggedInUserId: string,
     @Param('dir') dir: string,
     @Param('userId') userId: string,
-    @Param('postId') postId?: string,
+    @Query('postId') postId?: string,
   ): Promise<{ url: string }> {
     return await this.storageService.getDeleteUrl(
       loggedInUserId,
